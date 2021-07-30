6848ee981e7bb2bec4a07e3f80026063c78db339
thanhdo2206 <dovanducthanh@gmail.com>
2021-07-30 21:36:37 +0700

    up bai chuong 5

---
+++ module1/bai5_HTML_form/bai_tap/tao_survey_khach_hang/main.js
@@ -0,0 +1,48 @@
+
+var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+
+var other = $('.other');
+var maxLength = $('.max_length');
+var sex = $$('input[name= "sex"]');
+sex.forEach((item, index) => {
+    item.addEventListener('click', (event) => {
+        if (item.classList.contains('other')) {
+            maxLength.style.display = 'block';
+        }
+        else {
+            maxLength.style.display = 'none';
+
+        }
+
+    })
+})
+
+var otherText = $('.other_text');
+var lengthWord = $('.length_word');
+otherText.addEventListener('keyup', (event) => {
+    var text = event.target.value;
+    var lengthText =text.length;
+    lengthWord.innerText = lengthText;
+
+})
+
+var otherText = $('.other_text');
+otherText.addEventListener('click', () => {
+    maxLength.style.display = 'block';
+    other.checked = 'true';
+})
+
+
+
+var formItems = $$('.form_item');
+formItems.forEach((item, index) => {
+
+    item.addEventListener('click', (event) => {
+        $('.form_item.active').classList.remove('active');
+        item.classList.add('active');
+
+
+    })
+})
+
