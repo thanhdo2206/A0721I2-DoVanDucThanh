c1eb300bc858b358409b3208d6f56408b66202de
thanhdo2206 <dovanducthanh@gmail.com>
2021-08-10 11:04:45 +0700

    up bai chuong 8,9

---
+++ module1/bai9_cau_truc_dieu_kien_2/bai_tap/game_puzzle/main.js
@@ -0,0 +1,54 @@
+let imgs = document.querySelectorAll('.imgs');
+
+
+imgs.forEach((imgPart, index) => {
+    // bam qua cac hinh
+    let parts = imgPart.querySelectorAll('.part');
+    let partsLen = parts.length;
+    parts.forEach((img, index) => {
+        img.addEventListener('click', event => {
+            //chuyển đổi giữa các hình
+            img.classList.remove('active');
+            if (index == partsLen - 1) parts[0].classList.add('active');
+            else parts[index + 1].classList.add('active');
+
+            //đi tìm hình đúng
+
+            //lấy ra 1 mảng chứa hình ảnh có class active
+            let actives = document.querySelectorAll('.active');
+            // console.log(actives);
+
+            // lấy những class trong hình ảnh đầu tiên trong mảng actives
+            let active = actives[0].classList;
+            // console.log(active);
+
+            //lấy class thứ hai trong mảng active
+            let check = active[1];
+            // console.log(check);
+
+            // so sánh class để in ra kết quả
+            let count = 0;
+            for (let i = 1 ; i < actives.length ; i++){
+                let a = actives[i].classList;
+                let b = a[1];
+                if(check == b ) count++;
+            }
+            // console.log(count);
+            result(count);
+        })
+    })
+})
+
+let text = document.getElementById('text');
+function result(count) {
+    if (count == 4) {
+        text.innerText = "Đúng rồi bạn ơi";
+        document.getElementById('container').style.boxShadow = `10px 0px 10px 0px green`;
+    } else {
+        text.innerText = " ";
+        document.getElementById('container').style.boxShadow = `10px 0px 10px 0px #ccc`;
+
+    }
+}
+
+
