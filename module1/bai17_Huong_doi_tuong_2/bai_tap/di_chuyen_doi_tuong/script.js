b13aac9c02a90d196d8602061de16f6d336933f8
thanhdo2206 <dovanducthanh@gmail.com>
2021-09-08 21:04:53 +0700

    chuong 17

--- module1/bai17_Huong_doi_tuong_2/bai_tap/di_chuyen_doi_tuong/script.js
+++ module1/bai17_Huong_doi_tuong_2/bai_tap/di_chuyen_doi_tuong/script.js
@@ -10,67 +10,39 @@ function Hero(image, top, left, speed) {
   this.speed = speed;
 
   this.getHeroElement = function () {
-    return '<img width="' + this.size + '"' +
-      ' height="' + this.size + '"' +
-      ' src="' + this.image + '"' +
-      ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
-  }
+    return (
+      '<img width="' + this.size +'"' +
+      ' height="' +this.size +'"' +
+      ' src="' + this.image +'"' +
+      ' style="top: ' + this.top +"px; left:" + this.left + 'px;position:absolute;" />'
+    );
+  };
 
   this.moveRight = function () {
     this.left += this.speed;
-    console.log('ok: ' + this.left);
-  }
+    console.log("ok: " + this.left);
+  };
 
   this.moveDown = function () {
     this.top += this.speed;
-    console.log('ok: ' + this.top);
-  }
-
-  this.moveLeft = function () {
-    this.left -= this.speed;
-    console.log('ok: ' + this.left);
-  }
-
-  this.moveUp = function () {
-    this.top -= this.speed;
-    console.log('ok: ' + this.top);
-  }
+    console.log("ok: " + this.top);
+  };
 
 }
 
 let speedInput = parseInt(prompt("nhập tốc độ bạn muốn"));
-var hero = new Hero('lionel-messi-co-the-roi-barca-vao-nam-2021-4bf-5056926.jpg', 20, 30, speedInput);
+var hero = new Hero("lionel-messi-co-the-roi-barca-vao-nam-2021-4bf-5056926.jpg",20,30,speedInput);
 
-//dịch phải left lúc sau lớn hơn left lúc đầu
-//dịch trái left lúc sau bé hơn left lúc đầu
-let leftCurrent = 0;
-// hero.moveRight();
+
+let first = setInterval(start, 500);
 
 function start() {
-
   if (hero.left < window.innerWidth - hero.size) {
+    hero.moveRight();
+  } else if (hero.top < window.innerHeight - hero.size) {
+    hero.moveDown();
+  } else clearInterval(first);
 
-    if (leftCurrent < hero.left) {
-      leftCurrent = hero.left;
-      hero.moveRight();
-
-    }
-
-
-  } else if (hero.top < window.innerHeight - hero.size) hero.moveDown();
-  else if (hero.left !== 0) {
-    hero.moveLeft();
-    // leftCurrent = hero.left;
-  }
-  // else if (hero.top !== 0) hero.moveUp();
-
-
-  
-
-  document.getElementById('game').innerHTML = hero.getHeroElement();
-
-
-  setTimeout(start, 500);
+  document.getElementById("game").innerHTML = hero.getHeroElement();
 }
 
-start();
